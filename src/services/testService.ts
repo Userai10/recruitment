92978cdd68e86c4300b297e38842bd953d4ffcf7
Userai10 <mfeahr23@gmail.com>
2025-08-20 15:18:21 +0530

    Updated testService.ts

--- src/services/testService.ts
+++ src/services/testService.ts
@@ -54,7 +54,7 @@ export interface UserTestStatus {
 }
 export const testService = {
   getTestSettings: (): TestSettings => ({
-    testStartTime: new Date('2025-08-25T10:00:00'), // Fixed start time for all users
+    testStartTime: new Date('2025-08-20T15:22:00'), // Fixed start time for all users
     testDuration: 60, // 60 minutes
     maxTabSwitches: 5,
     isTestActive: true
